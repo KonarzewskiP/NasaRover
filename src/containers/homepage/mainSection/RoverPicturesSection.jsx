9ac52d723b99625c37596a13d2a9a8062f7518b1
KonarzewskiP <pawelkonarzewski13@gmail.com>
2021-02-13 15:58:26 +0000

    Implement cache.

--- src/containers/homepage/mainSection/RoverPicturesSection.jsx
+++ src/containers/homepage/mainSection/RoverPicturesSection.jsx
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
 import MarsPicture from "./marsPicture";
 import axios from "axios";
 import Pagination from "../../../components/pagination";
+import useSWR from 'swr';
 
 const PicturesContainer = styled.div`
   display: flex;
@@ -12,7 +13,7 @@ const PicturesContainer = styled.div`
   justify-items: center;
   align-self: center;
   color: white;
-  margin: 2.5rem ;
+  margin: 2.5rem;
   overflow: auto;
 `
 const LoadingTitle = styled.h2`
@@ -32,54 +33,39 @@ const PaginationButtonsWrapper = styled.div`
 const ImageContainer = styled.div`
   max-height: 90%;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(180px,  1fr));
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
   grid-gap: 1rem;
   justify-items: center;
 `
 
+const fetcher = (API) => axios.get(API)
+    .then(response => {
+        return response.data;
+    });
+
 const RoverPicturesSection = ({apiData, openModal, setModalPicture}) => {
     const API_KEY = "kVBXO7R76SwS8bbEwtYcNo7zlXn5bmxmZc2Bf7ns";
-    const API = `https://api.nasa.gov/mars-photos/api/v1/rovers/${apiData.roverName}/photos?sol=${apiData.marsSol}&camera=${apiData.activeRoverCam}&api_key=${API_KEY}`
+    const API = `https://api.nasa.gov/mars-photos/api/v1/rovers/${apiData.roverName}/photos?sol=${apiData.marsSol}&api_key=${API_KEY}`
 
-    const [marsPictures, setMarsPictures] = useState([]);
-    const [loading, setLoading] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
     const [picturesPerPage] = useState(15);
 
-
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            await axios.get(API)
-                .then(res => {
-                    setMarsPictures(res.data.photos);
-                })
-                .catch(err => console.log(err));
-            setLoading(false);
-        }
-        fetchData();
-    }, [apiData]);
+    const {data, error} = useSWR(API, fetcher);
 
     // Get current posts
     const indexOfLastPicture = currentPage * picturesPerPage;
     const indexOfFirstPicture = indexOfLastPicture - picturesPerPage;
-    const currentPictures = marsPictures.slice(indexOfFirstPicture, indexOfLastPicture);
+    const currentPictures = (data ? data.photos.slice(indexOfFirstPicture, indexOfLastPicture) : []);
+    const marsPictures = (data ? data.photos : []);
 
     //Change page
     const paginate = (pageNumber) => {
         setCurrentPage(pageNumber)
     }
 
-    if (loading) {
-        return <LoadingTitle>Loading...</LoadingTitle>
-    }
-
-    if (apiData && marsPictures.length === 0) {
-        return <LoadingTitle>No Data</LoadingTitle>
-    }
-
-
-    console.log(apiData)
+    if (error) return <LoadingTitle>failed to load</LoadingTitle>
+    if (!data) return <LoadingTitle>Loading...</LoadingTitle>
+    if (data.photos.length === 0) return <LoadingTitle>No Data...</LoadingTitle>
 
     return (
         <PicturesContainer>
